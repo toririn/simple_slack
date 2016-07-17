b170e1fab69c7a6e4181d3b8f978ba7d806f26d4
toririn <toririn.paftako@gmail.com>
2016-07-17 17:15:14 +0900

    add client rspec

--- spec/simple_slack_spec.rb
+++ spec/simple_slack_spec.rb
@@ -1,11 +1,35 @@
 require 'spec_helper'
 
 describe SimpleSlack do
-  it 'has a version number' do
-    expect(SimpleSlack::VERSION).not_to be nil
+  let(:client) { SimpleSlack::Client.new(SIMPLE_SLACK_API_TOKEN) }
+
+  describe 'VERSION' do
+    it 'has a version number' do
+      expect(SimpleSlack::VERSION).not_to be nil
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#client' do 
+    it 'success object make' do
+      expect(client).to be_truthy
+    end
+  end
+
+  describe '#get' do
+    it 'is SimpleSlack::Getter class' do
+      expect(client.get.class).to eq SimpleSlack::Getter
+    end
+  end
+
+  describe '#post' do
+    it 'is SimpleSlack::Poster class' do
+      expect(client.post.class).to eq SimpleSlack::Poster
+    end
+  end
+
+  describe '#bot' do
+    it 'is SimpleSlack::Botter class' do
+      expect(client.bot.class).to eq SimpleSlack::Botter
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+SIMPLE_SLACK_API_TOKEN = ENV['GEM_SIMPLE_SLACK_API_TOKEN']
 require 'simple_slack'
